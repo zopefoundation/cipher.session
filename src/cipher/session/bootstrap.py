388b34e87d4e32c6edf3dbb58d6d333f0b69a686
Adam Groszer <agroszer@mail.com>
2012-07-21 09:24:20 +0000

    Now really get rid of ISessionDataContainer on bootstrap.

--- src/cipher/session/bootstrap.py
+++ src/cipher/session/bootstrap.py
@@ -22,9 +22,11 @@ import zope.component
 from zope.app.appsetup.bootstrap import getInformationFromEvent
 from zope.app.appsetup.bootstrap import addConfigureUtility
 from zope.processlifetime import IDatabaseOpenedWithRoot
+from zope.session.interfaces import ISessionDataContainer
 
 from cipher.session import interfaces
 from cipher.session.session import SessionDataManager
+
 
 @zope.component.adapter(IDatabaseOpenedWithRoot)
 def bootStrapSessionDataManager(event):
@@ -37,8 +39,26 @@ def bootStrapSessionDataManager(event):
     """
 
     db, connection, root, root_folder = getInformationFromEvent(event)
+
     try:
+        # now first get rid of any ISessionDataContainer
         sm = root_folder.getSiteManager()
+        utils = [reg for reg in sm.registeredUtilities()
+                 if reg.provided.isOrExtends(ISessionDataContainer)
+                    and reg.name == '']
+        if utils:
+            # check our assumptions: there's only one, registered as a component
+            assert len(utils) == 1
+            assert utils[0].factory is None
+            utility = utils[0].component
+            sm.unregisterUtility(utility, ISessionDataContainer)
+
+            try:
+                del utility.__parent__[utility.__name__]
+            except:
+                pass
+            transaction.commit()
+
         utils = [reg for reg in sm.registeredUtilities()
                  if reg.provided.isOrExtends(interfaces.ISessionDataManager)
                     and reg.name == '']
@@ -50,6 +70,11 @@ def bootStrapSessionDataManager(event):
             if isinstance(utility, SessionDataManager):
                 return  # nothing to do
             sm.unregisterUtility(utility, interfaces.ISessionDataManager)
+
+            try:
+                del utility.__parent__[utility.__name__]
+            except:
+                pass
 
         addConfigureUtility(
             root_folder,
