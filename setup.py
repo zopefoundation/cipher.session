ab0792da5c98082836b8e5135a0fc76b7cf02b95
Adam Groszer <agroszer@gmail.com>
2016-04-18 14:54:29 +0200

    Back to development: 2.1.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def alltests():
 
 setup(
     name='cipher.session',
-    version='2.1.0',
+    version='2.1.1.dev0',
     url="http://pypi.python.org/pypi/cipher.session/",
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
