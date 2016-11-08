230f43e131e9c6a1fbeb07a2f0373fad187acf76
Adam Groszer <agroszer@gmail.com>
2016-11-08 14:27:50 +0100

    bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def alltests():
 
 setup(
     name='cipher.session',
-    version='2.1.1.dev4',
+    version='2.1.1.dev5',
     url="http://pypi.python.org/pypi/cipher.session/",
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
